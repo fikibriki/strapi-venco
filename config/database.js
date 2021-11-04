3c73f147fe210722475becd5ccaad66a16207f57
Filip Lazovski <filip_lazovski@hotmail.com>
2021-11-04 15:09:36 +0100

    fix

--- config/database.js
+++ config/database.js
@@ -3,7 +3,24 @@ module.exports = ({ env }) => {
 
 
   if(env('NODE_ENV')=== 'production'){
-    const config = prase(process.env.NODE_ENV);
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: 'default',
+      connections: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+        },
+        options: {
+          ssl: false,
+        }
+      }
+    }
   }
   return {
     defaultConnection: 'default',
